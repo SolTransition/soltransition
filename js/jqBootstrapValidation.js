ad4f1685ec9d992bce8381b874ed0b142b16e3ff
SolTransition <soltransition7@gmail.com>
2025-08-28 22:38:21 -0400

    Update jqBootstrapValidation.js

--- js/jqBootstrapValidation.js
+++ js/jqBootstrapValidation.js
@@ -3,7 +3,7 @@
  *
  * v1.3.6
  *
- * License: MIT <http://opensource.org/licenses/mit-license.php> - see LICENSE file
+ * License: MIT <https://opensource.org/licenses/mit-license.php> - see LICENSE file
  *
  * http://ReactiveRaven.github.com/jqBootstrapValidation/
  */
